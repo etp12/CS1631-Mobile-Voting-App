b36cd93fe97adf92aa7ac82b567bf70aa5eed84a
Ethan Pavolik <etp12@pitt.edu>
2017-02-21 16:11:26 -0500

    Component receives input from PrjRemote, but not sending back correctly

--- VotingComponent.java
+++ VotingComponent.java
@@ -2,30 +2,59 @@
 * etp12
 * Receives message from SIS Server to record votes.
 **/
-import java.net.Socket;
-public class VotingComponent {
-	public static void main(String[] args) {
-		try {
-			Socket sock = new Socket("127.0.0.1", 53217);
-			MsgDecoder decoder = new MsgDecoder(sock.getInputStream());
 
-			KeyValueList kvl = new KeyValueList();
-			kvl.putPair("MsgID", "21");
-			kvl.putPair("Name", "VotingComponent");
+import java.io.*;
 
-			MsgEncoder encoder = new MsgEncoder(sock.getOutputStream());
-			encoder.sendMsg(kvl);
-			KeyValueList reply = decoder.getMsg();
-			if (reply != null) {
-				System.out.println("Reply received..");
-				System.out.println(reply.getValue("MsgID"));
-			} else {
-				System.out.println("No reply!");
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+public class VotingComponent implements ComponentBase{
+
+private final int init=0;
+private final int success=1;
+private final int failure=2;
 
 
+private int state;
+
+public VotingComponent(){
+    state=init;
+}
+
+/* just a trivial example */
+
+private void doAuthentication(String first,String last,String passwd){
+if (first != null && last != null && passwd != null) {
+	if (first.equals("Ethan")&&last.equals("Pavolik")&&passwd.equals("etp12"))
+    state=success;
+}
+else {
+	System.out.println("First: " + first);
+	System.out.println("Last: " + last);
+	System.out.println("passwd: " + passwd);
+
+	  state=failure;
 	}
+}
+
+/* function in interface ComponentBase */
+
+public KeyValueList processMsg(KeyValueList kvList){
+    int MsgID=Integer.parseInt(kvList.getValue("MsgID"));
+    if (MsgID!=702) return null;
+    doAuthentication(kvList.getValue("FirstName"),kvList.getValue("LastName"),kvList.getValue("passwd"));
+    KeyValueList kvResult = new KeyValueList();
+    kvResult.addPair("MsgID","1");
+    kvResult.addPair("Description","Authentication Result");
+
+   switch (state) {
+   case success: {
+      kvResult.addPair("Authentication","success");
+      break;
+   }
+  case failure: {
+     kvResult.addPair("Authentication","failure");
+     break;
+   }
+   }
+  return kvResult;
+}
+
 }
